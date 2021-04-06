3e805c4ba47ab7ef60f31d69278b613dd873151a
Erik <erikio93@hotmail.com>
2021-04-06 16:53:02 +0200

    added instruction comment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+/* USESTATE SKA IN HÄR DEN SKA INNEHÅLLA EN INITIAL STATE SOM HAR DET FÖRSTA KORTET, 
+SEN SKA DEN "FYLLAS" PÅ OCH LOOPA FRAM DE NYA KORTEN MAN LÄGGER TILL */
+
 export default function Home() {
   return (
     <div>
