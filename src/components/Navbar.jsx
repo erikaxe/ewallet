40ecd3ed2f762e10052307bb09852b2b20e336d5
Erik <erikio93@hotmail.com>
2021-04-06 16:54:26 +0200

    Added Router links & bootstrap styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="bg-primary">
-      <h1>Navbar component Works</h1>
+    <div className="navbar bg-primary d-flex justify-content-around align-items-center">
+      <h3>Navbar</h3>
+      <ul className="links d-flex justify-content-around">
+        <Link to="/">
+          <li className="text-danger">Home</li>
+        </Link>
+        <Link to="/addcard">
+          <li className="text-danger">Add card</li>
+        </Link>
+      </ul>
     </div>
   );
 }
