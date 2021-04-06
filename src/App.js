c02f9ca5a1005ca262cb4fb1ec92ef8e6015ab3f
Erik <erikio93@hotmail.com>
2021-04-06 16:52:18 +0200

    Fixed routing, routing now raenders components as intended, URL updates as intended

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AddCard from "./components/AddCard";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header"></header> */}
-      <Navbar />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/addcard" component={AddCard} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
