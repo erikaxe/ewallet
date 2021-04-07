c0341d64e9b2579e0cc160de9b3a86292740793f
Erik <erikio93@hotmail.com>
2021-04-07 12:11:58 +0200

    added form section with all required inputs, + one state for each input exept CCV input, it wont display on the card. All interpolated variables in preview need to be placed on correct position

--- src/components/AddCard.jsx
+++ src/components/AddCard.jsx
@@ -1,9 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AddCard() {
+  // card number state
+  const [numberState, setNumberState] = useState("xxxx xxxx xxxx xxxx");
+  // name state
+  const [nameState, setNameState] = useState("ANDERS ANDERSSON");
+  // valid state
+  const [validState, setValidState] = useState("xx / xx");
+  // card type state
+  const [cardState, setCardState] = useState("VISA");
+
   return (
-    <div>
-      <h1>AddCard component Works</h1>
+    <div className="container">
+      <h2 className="text-center mt-5">ADD A NEW BANK CARD</h2>
+      <div className="bank-card">
+        <h3>{cardState}</h3>
+        <p>{numberState}</p>
+        <p>{nameState}</p>
+        <p>{validState}</p>
+      </div>
+      {/*!!!!!!!!!!!! FORM STARTS, FIX FORM!!!!!!!!!!*/}
+
+      <form>
+        <div className="group">
+          <label htmlFor="number">CARD NUMBER</label> <br />
+          <input
+            type="number"
+            name="number"
+            onChange={(e) => {
+              const cardNumber = e.target.value;
+              setNumberState(cardNumber);
+            }}
+          />
+        </div>
+
+        <div className="group">
+          <label htmlFor="holder">CARD HOLDER</label> <br />
+          <input
+            type="text"
+            name="holder"
+            onChange={(e) => {
+              const cardName = e.target.value;
+              setNameState(cardName);
+            }}
+          />
+        </div>
+        <div className="group">
+          <label htmlFor="valid">VALID THRU</label> <br />
+          <input
+            type="number"
+            name="valid"
+            onChange={(e) => {
+              const cardValid = e.target.value;
+              setValidState(cardValid);
+            }}
+          />
+        </div>
+        <div className="group">
+          <label htmlFor="ccv">CCV</label> <br />
+          <input type="number" name="ccv" />
+        </div>
+        <div className="group">
+          <label htmlFor="vendor">VENDOR</label> <br />
+          <select
+            type="text"
+            name="vendor"
+            onChange={(e) => {
+              const selectedCard = e.target.value;
+              setCardState(selectedCard);
+            }}
+          >
+            <option value="" disabled selected hidden>
+              Choose a card...
+            </option>
+            <option value="MasterCard">Mastercard</option>
+            <option value="VISA">VISA</option>
+            <option value="American Express">American Express</option>
+          </select>
+        </div>
+        <button className="btn btn-secondary mt-5">ADD CARD</button>
+      </form>
+
+      {/*!!!!!!!!!!!! FORM ENDS,  FIX FORM!!!!!!!!!!*/}
     </div>
   );
 }
