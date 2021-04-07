ac1b218fc0cdefdc87dbde101ddab4cac8abc932
Erik <erikio93@hotmail.com>
2021-04-07 16:43:34 +0200

    added initial state, + instruction comment

--- src/components/cardsSlice.jsx
+++ src/components/cardsSlice.jsx
@@ -3,7 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 const slice = createSlice({
   name: "cards",
 
-  initialState: {},
+  /* !!!!!!!!! INITIAL STATE SKA VISAS SOM ACTIVE CARD DIREKT, 
+  EV GÖR ETT NYTT STATE DÄR ALLA NYA KORTEN HAMNAR NÄR MAN
+  SKAPAR DE OCH LOOPA FRAM DEN UNDER ICKE ACTIVE CARDS !!!!!!!!!*/
+
+  initialState: [
+    {
+      number: "1111 1111 1111 1111",
+      name: "ERIK AXELSSON",
+      valid: "10 / 24",
+      card: "VISA",
+    },
+  ],
 
   reducers: {},
 
