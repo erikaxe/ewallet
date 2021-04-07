ac06b1f5e1bb045d1493fbfd807cece3cb57ec6c
Erik <erikio93@hotmail.com>
2021-04-07 10:11:56 +0200

    created basic bootstrap style for home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 /* USESTATE SKA IN HÄR DEN SKA INNEHÅLLA EN INITIAL STATE SOM HAR DET FÖRSTA KORTET, 
 SEN SKA DEN "FYLLAS" PÅ OCH LOOPA FRAM DE NYA KORTEN MAN LÄGGER TILL */
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home component Works</h1>
+    <div className="container">
+      <h1 className="text-center mt-5">E-WALLET</h1>
+      <p className="text-center mt-5">CURRENT ACTIVE CARD</p>
+      <div className="text-center mt-5">
+        !!!!!!!!!!!!!!!!!! INSERT ACTIVE CARD FROM STATE HERE
+        !!!!!!!!!!!!!!!!!!!!
+      </div>
+      <hr />
+      <p className="text-center mt-5">DEACTIVATED CARDS</p>
+      <div className="text-center mt-5">
+        !!!!!!!!!!!!!!!!!! INSERT NEW CARDS HERE !!!!!!!!!!!!!!!!!!!!
+      </div>
+      <Link to="/addcard">
+        <button className="btn btn-dark">ADD A NEW CARD</button>
+      </Link>
     </div>
   );
 }
