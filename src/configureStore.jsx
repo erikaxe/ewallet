5f964c31d45b630f76fbcf2c79e994f49ee7426c
Erik <erikio93@hotmail.com>
2021-04-08 18:14:53 +0200

    removed comment

--- src/configureStore.jsx
+++ src/configureStore.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CardsReducer from "./components/cardsSlice";
-/* IMPORT REDUCER HÄR */
+
 export default configureStore({
   reducer: {
     cards: CardsReducer,
