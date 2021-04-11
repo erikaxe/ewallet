3def0ea943b1c258394a751b21cc9d21c66dbd08
Erik <erikio93@hotmail.com>
2021-04-11 02:15:21 +0200

    validation works for all inputs, deleted old code

--- src/components/AddCard.jsx
+++ src/components/AddCard.jsx
@@ -11,15 +11,11 @@ export default function AddCard() {
   // name state
   const [name, setName] = useState(""); /* ANDERS ANDERSSON */
   // expiry state
-  const [expiry, setExpiry] = useState("1218"); /* xx / xx */
+  const [expiry, setExpiry] = useState(""); /* xx / xx */
   // cvc state
   const [cvc, setCvc] = useState("");
   // package state for styling and animation
   const [focus, setFocus] = useState("");
-
-  // MMYY error state
-  const [error, setError] = useState("");
-  console.log(error);
 
   const dispatch = useDispatch();
 
@@ -32,7 +28,7 @@ export default function AddCard() {
         name: name,
         expiry: expiry,
         cvc: cvc,
-        id: Date.now(),
+        /* id: Date.now(), */
         status: false,
       })
     );
@@ -43,64 +39,41 @@ export default function AddCard() {
     setCvc("");
   };
 
-  /* const validate = (number) => {
-    let digits = number.toString().split("").map(Number);
-
-    // if it's an even number
-    if (digits.length % 2 === 0) {
-      digits = digits.map((digit, i) => (i % 2 === 0 ? digit * 2 : digit));
-    } else {
-      digits = digits.map((digit, i) => (i % 2 === 1 ? digit * 2 : digit));
-    }
-    console.log(digits);
-  }; */
-
-  /* let validateExpire = () => {
-    console.log(value);
-    if (value.expiry) {
-      expiry = "expire date required";
-    } else if ([0 - 9] + [0 - 9]) {
-      expiry = "expire date is invalid";
-    }
-  }; */
-
-  /* function validDate(value) {
-    let result = false;
-    value = value.split("/");
-    let pattern = /^\d{2}$/;
-
-    if (value[0] < 1 || value[0] > 12) result = true;
-
-    if (!pattern.test(value[0]) || !pattern.test(value[1])) result = true;
-
-    if (value[2]) result = true;
-
-    if (result) alert("Please enter a valid date in MM/YY format.");
-  } */
-
   const validate = (string) => {
     // year must be atleast 21
-    const yearLimit = 21; // or pass this as an argument if you want
+    /* const yearLimit = 21; */ // or pass this as an argument if you want
 
     // MMYY must match this
-    const regex = /^(?:0[1-9]|1[0-2])(\d{2})$/;
+    const regex = /^(0[1-9]|1[0-2])(\d{2})$/;
+    const today = new Date();
+    console.log(today.getMonth());
     const match = string.match(regex);
     if (!match) {
-      setError("Please enter a valid expire date");
       return false;
     }
-    const year = Number(match[1]);
-    return year >= yearLimit;
+
+    // match the first group in the regex
+    const month = Number(match[1]);
+    // match the second group in the regex
+    const year = Number(match[2]);
+
+    return "20" + year >= today.getFullYear() && month >= today.getMonth() + 1;
   };
 
-  console.log(
-    validate(expiry)
-    /* validate("1234"),
-    validate("1212"),
-    validate("0101"),
-    validate("0118"),
-    validate("0119") */
-  );
+  const validateCvc = (string) => {
+    const regex = /^\d{3}$/;
+    return regex.test(string);
+  };
+
+  const validateName = (string) => {
+    const regex = /^[^0-9]{2,}$/;
+    return regex.test(string);
+  };
+
+  const validateNumber = (string) => {
+    const regex = /^([4-5][0-9]|6[025])\d{14}$/;
+    return regex.test(string);
+  };
 
   return (
     <div className="container">
@@ -211,17 +184,16 @@ export default function AddCard() {
           name="expiry"
           placeholder="MMYY"
           value={expiry}
-          /* onClick={(e) => validDate(e.target.value)} */
           onChange={(e) => setExpiry(e.target.value)}
           onFocus={(e) => setFocus(e.target.name)}
           minLength="4"
           maxLength="4"
         />
-        <p>{error}</p>
-        {/* <button onClick={() => validDate(expiry)}>Validate expire date</button> */}
         <label htmlFor="cvc">Cvc</label>
         <input
           type="tel"
+          min="001"
+          max="999"
           name="cvc"
           placeholder="CVC (must be 3 digits)"
           value={cvc}
@@ -236,12 +208,10 @@ export default function AddCard() {
           className="btn btn-secondary mt-5"
           value="ADD CARD"
           disabled={
-            number.length < 16 ||
-            name.length < 2 ||
-            /* expiry.length < 4 || */
-            cvc.length < 3 ||
-            // validate fungerar inte som tänkt
-            validate(expiry)
+            !validateNumber(number) ||
+            !validateName(name) ||
+            !validateCvc(cvc) ||
+            !validate(expiry)
           }
         >
           ADD CARD
