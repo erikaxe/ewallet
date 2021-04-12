8e9867f26c213abce30d5c7d1d0cc8024d8cb38c
Erik <erikio93@hotmail.com>
2021-04-12 13:54:53 +0200

    added bootstrap class to remove margin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="navbar bg-primary d-flex justify-content-around align-items-center">
-      <h3>Navbar</h3>
-      <ul className="links d-flex justify-content-around">
+      <ul className="links d-flex justify-content-around m-0">
         <Link to="/">
           <li className="btn btn-secondary">Home</li>
         </Link>
